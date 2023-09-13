8525e455320cf422684d752915001e7606472b3b
Fatima <ffatimairshad@outlook.com>
2023-09-13 15:10:22 +0300

    Add project files.

---
+++ FullstackApi/Data/BooksLibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullstackApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullstackApi.Data
+{
+    public class BooksLibraryDbContext : DbContext
+    {
+        public BooksLibraryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
---
+++ FullstackApi/Models/Books.cs
@@ -0,0 +1,11 @@
+﻿namespace FullstackApi.Models
+{
+    public class Books
+    {
+        public Guid Id { get; set; }
+
+        public string BName { get; set; }
+
+        public string Author { get; set; }
+    }
+}
